3a3e81c3c46d4a90d54305115fb8488632e9f126
RafaelAnt <rafael.uminho@hotmail.com>
2015-10-24 21:01:33 +0100

    continuação

--- quicksort.c
+++ quicksort.c
@@ -3,10 +3,40 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define ARRAY_SIZE 1024*1024*50
+#define ARRAY_SIZE 100000000 //100 mil elementos
 
 int * array;
 
+int test (long double sum) {
+//test variables
+  long long int sumF=0;
+  int lastI=-1;
+  int correct=1;
+  int i=0;
+
+  lastI=-1;
+  while(i<ARRAY_SIZE && correct==1){
+  	sumF += array[i];
+    if (array[i] < lastI) correct=0;
+    lastI=array[i];
+  	i++;
+  }
+
+  printf("Sumatório depois de ordenação: %lld\n",sumF);
+  if (sum!=sumF) correct=0;
+
+  return correct;
+}
+
+void printArray(){
+  printf("[%d",array[0]);
+  int i=1;
+  while(i<ARRAY_SIZE){
+    printf(", %d",array[i]);
+    i++;
+  }
+  printf("]\n");
+}
 
 void quicksort(int lo,int hi){
 
@@ -16,17 +46,18 @@ void quicksort(int lo,int hi){
 
   //partition
   do{
-    #pragma	omp parallel{
-      #pragma omp sections{
-        #pragma omp section{
-          while(array[i] < x){i++;}
-        }
-        #pragma omp section{
-          while(array[j] > x){j--;}
-        }
-      }
-    }
-    #pragma omp barrier
+    /* #pragma omp parallel sections
+     {
+       #pragma omp section
+       {*/
+           while(array[i]<x) i++;
+       /*}
+       #pragma omp section
+       {*/
+           while(array[j]>x) j--;
+      /* }
+     #pragma omp barrier
+   }*/
 
     if(i<=j){
       h=array[i];
@@ -45,16 +76,33 @@ void quicksort(int lo,int hi){
 
 
 int main (){
+
+  printf("\n\n*******************************************************\n\n");
+
   int i=0;
+  long long int sum =0;
   srand(time(NULL));
 
   array=malloc( sizeof(int) * ARRAY_SIZE); //5MB
 
+  printf("A inicializar o array com %d elementos...\n", ARRAY_SIZE);
+
   while(i<ARRAY_SIZE){
-    array[i] = rand() % 100 + 1;
+    array[i] = rand() % 80 + 1;
+    sum += array[i];
     i++;
   }
 
+  printf("Concluido!\n");
+  printf("Tamanho do array: ");
+  double bytes = sizeof(int) * ARRAY_SIZE;
+  if(bytes<=1024)  printf("%.3f bytes...\n", (double) bytes);
+  if(bytes>1024 && bytes <= 1024*1024)  printf("%.3f Kbytes...\n", (double) bytes/1024);
+  if(bytes>1024*1024)  printf("%.3f Mbytes...\n", (double) bytes/(1024*1024));
+
+
+  printf("Sumatório depois de ordenação: %lld\n",sum);
+  printf("A correr o quicksort...\n");
   clock_t start = clock(); //inicio contagem do tempo
 
   quicksort(0,ARRAY_SIZE);
@@ -62,11 +110,17 @@ int main (){
   clock_t end = clock();  //fim da contagem do tempo
   float seconds = (float)(end - start) / CLOCKS_PER_SEC;
 
-  printf("\n\nExecutado em %f segundos...\n\n",seconds);
+  printf("Concluido!\n");
+  printf("Executado em %f segundos.\n",seconds);
+  printf("A iniciar função de teste...\n");
 
-  /*i=0;
-  while(i<1024*1024*5){
-    printf("%d ; ",array[i]);
-    i++;
-  }*/
+  int r=test(sum);
+
+  printf("Concluido!\n");
+  if (r==0) printf("Incorreto\n");
+  if (r==1) printf("Correto\n");
+
+
+  printf("\n\n*******************************************************\n\n");
+
 }
